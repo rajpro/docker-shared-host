d8649068d1c35e577552e7f3d36941c3519e48b3
Rajesh Sardar <rajeshkumarsardar@gmail.com>
2019-05-05 12:10:37 +0530

    changes in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SENTORA_INSTALLER_VERSION="master"
+ROUNDPANEL_INSTALLER_VERSION="1.0.0"
 SENTORA_CORE_VERSION="1.0.1"
 
 PANEL_PATH="/etc/sentora"
@@ -10,18 +10,18 @@ PANEL_CONF="$PANEL_PATH/configs"
 if [ -f /etc/sentora/panel/index.php ]; then
     break;
 else
-    apk add mysql-client
+    apt-get install -y mysql-client
     mkdir -p /etc/sentora/panel && mkdir -p /etc/sentora/configs
-    wget -nv -O sentora_core.zip https://github.com/sentora/sentora-core/archive/$SENTORA_CORE_VERSION.zip
+    wget -nv -O roundpanel.zip https://github.com/rajpro/roundpanel/archive/$ROUNDPANEL_CORE_VERSION.zip
     chown -R root:root $PANEL_PATH
     unzip -oq sentora_core.zip -d $PANEL_PATH
-    /bin/cp -rf $PANEL_PATH/sentora-core-$SENTORA_CORE_VERSION/* $PANEL_PATH/panel/
-    rm sentora_core.zip
+    /bin/cp -rf $PANEL_PATH/roundpanel-$ROUNDPANEL_CORE_VERSION/* $PANEL_PATH/panel/
+    rm roundpanel.zip
     wget -nv -O sentora_preconfig.zip https://github.com/sentora/sentora-installers/archive/$SENTORA_INSTALLER_VERSION.zip
     unzip -oq sentora_preconfig.zip
     /bin/cp -rf sentora-installers-$SENTORA_INSTALLER_VERSION/preconf/* $PANEL_CONF
     rm sentora_preconfig*
     rm -rf sentora-*
-    rm -rf /etc/sentora/sentora-*
+    rm -rf /etc/sentora/roundpanel-*
     
 fi
